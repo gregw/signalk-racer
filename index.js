375d661ebc20c744ce27502b3f405694afe3b07d
Greg Wilkins <gregw@webtide.com>
2025-06-06 18:09:15 +1000

    WIP

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ module.exports = (app) => {
             lineStarboard: {
                 type: 'string',
                 title: 'Line starboard waypoint name',
-                default: 'boat'
+                default: 'startBoat'
             },
             linePort: {
                 type: 'string',
                 title: 'Line port waypoint name',
-                default: 'pin'
+                default: 'startPin'
+            },
+            period: {
+                type: 'number',
+                title: 'Update period in micro seconds',
+                default: 1000
             },
             speedToLine: {
                 type: 'number',
@@ -32,15 +37,156 @@ module.exports = (app) => {
         }
     }
 
+    let unsubscribes = []
+
     const plugin = {
         id: 'signalk-racer',
         name: 'SignalK Racing Plugin',
-        start: (settings, restartPlugin) => {
-            // start up code goes here.
+        start: (options, restartPlugin) => {
+            if (!options.linePort || !options.lineStarboard) {
+                app.error('Missing waypoint names: linePort and/or lineStarboard not configured.');
+                return;
+            }
+            app.debug('linePort:' + options.linePort);
+            app.debug('lineStarboard:' + options.lineStarboard);
+            app.debug('app.selfId:' + app.selfId);
+            app.debug('getSelfPath(design)' + JSON.stringify(app.getSelfPath('design')));
+
+            let state = {
+                wayPointsScanned: false
+            }
+
+            function sendDelta(path, value) {
+                const delta = {
+                    context: 'vessels.self',
+                    updates: [
+                        {
+                            source: { label: 'signalk-racer' },
+                            timestamp: new Date().toISOString(),
+                            values: [
+                                {
+                                    path: path,
+                                    value: value
+                                }
+                            ]
+                        }
+                    ]
+                }
+                app.handleMessage('vessels.self', delta)
+            }
+
+            function waypointToPosition(waypoint) {
+                if (
+                    waypoint &&
+                    waypoint.feature &&
+                    waypoint.feature.geometry &&
+                    waypoint.feature.geometry.type === 'Point' &&
+                    Array.isArray(waypoint.feature.geometry.coordinates) &&
+                    waypoint.feature.geometry.coordinates.length === 2
+                ) {
+                    const [longitude, latitude] = waypoint.feature.geometry.coordinates;
+                    return { latitude, longitude };
+                } else {
+                    return null;
+                }
+            }
+
+            function scanWaypoints() {
+                if (!state.wayPointsScanned) {
+                    app.resourcesApi.listResources(
+                        'waypoints',
+                        {}
+                    ).then(data => {
+                        state.wayPointsScanned = true;
+                        app.debug(JSON.stringify(data));
+                        for (const [key, value] of Object.entries(data)) {
+                            app.debug(`Key: ${key}, Value:`, JSON.stringify(value));
+
+                            if (value.name === options.linePort) {
+                                pos = waypointToPosition(value);
+                                if (pos) {
+                                    app.debug('linePort.position:' + pos);
+                                    state.linePort = pos;
+                                    sendDelta('racing.startLinePort', pos);
+                                }
+                            }
+                            if (value.name === options.lineStarboard) {
+                                pos = waypointToPosition(value);
+                                app.debug('lineStarboard.position:' + pos);
+                                state.lineStarboard = pos;
+                                sendDelta('racing.startLineStb', pos);
+                            }
+                        }
+                    }).catch(err => {
+                        app.error(err);
+                    });
+                }
+            }
+
+            let positionSubscription = {
+                context: 'vessels.' + app.selfId,
+                subscribe: [
+                    {
+                        path: 'navigation.position',
+                        period: options.period,
+                    }
+                ]
+            }
+
+            app.subscriptionmanager.subscribe(
+                positionSubscription,
+                unsubscribes,
+                (subscriptionError) => {
+                    app.error('Error:' + subscriptionError)
+                },
+
+                (delta) => {
+                    delta.updates.forEach((u) => {
+                        app.debug(JSON.stringify(u));
+                        u.values.forEach((v) =>
+                        {
+                            app.debug('POSITION ' + v.path + ' = ' + JSON.stringify(v.value))
+                            scanWaypoints();
+                        })
+                    })
+                }
+            )
+
+            let waypointSubscription = {
+                context: 'vessels.' + app.selfId,
+                subscribe: [
+                    {
+                        path: 'resources.waypoints.*', // Get all paths
+                        policy: 'instant'
+                    }
+                ]
+            }
+
+            app.subscriptionmanager.subscribe(
+                waypointSubscription,
+                unsubscribes,
+                (subscriptionError) => {
+                    app.error('Error:' + subscriptionError)
+                },
+                (delta) => {
+                    delta.updates.forEach((u) => {
+                        app.debug(JSON.stringify(u));
+                        u.values.forEach((v) =>
+                        {
+                            app.debug("WAYPOINT: " + v.path + ' = ' + JSON.stringify(v.value))
+                        })
+                    });
+                    state.wayPointsScanned = false;
+                    scanWaypoints();
+                }
+            )
         },
+
         stop: () => {
-            // shutdown code goes here.
+            unsubscribes.forEach((f) => f())
+            unsubscribes = []
         },
+
         schema: () => racerSchema
     };
 
